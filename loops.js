dd39d61f52b5eb45eb31483af78c5e3f87b87f0c
sadekul-ai <sadekul.psl@gmail.com>
2025-07-14 09:06:13 +0600

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,83 @@
+
+// full form of varriable name
+
+// var sweetGiven = 1;
+// while(sweetGiven <=5){
+//     console.log("Sweet Quality is Good");
+//     sweetGiven++
+// }
+
+// var sweetGiven = 0;
+// while(sweetGiven<=10){
+//     console.log(sweetGiven);
+//     sweetGiven++
+// }
+
+// var evenNumber = 0;
+// while(evenNumber <=10 ){
+//     console.log(evenNumber);
+//     evenNumber += 2;
+// }
+
+// var oddNumber = 1;
+// while(oddNumber <=10 ){
+//     console.log(oddNumber);
+//     oddNumber += 2;
+// }
+
+// for(var sweetGiven = 1; sweetGiven <= 6; sweetGiven++){
+//     console.log("Sweet Quality is Good")
+// }
+
+// for(var sweetGiven = 0; sweetGiven <= 10; sweetGiven++){
+//     console.log(sweetGiven)
+// }
+
+// for(var evenNumber = 0; evenNumber <= 10; evenNumber +=2 ){
+//     console.log(evenNumber)
+// }
+
+// for(var oddNumber = 1; oddNumber <= 10; oddNumber +=2 ){
+//     console.log(oddNumber)
+// }
+
+// short form of varriable name
+
+// for(var sG = 0; sG <= 10; sG++){
+//     console.log(sG)
+// }
+
+// for(var eN = 0; eN <= 10; eN +=2){
+//     console.log(eN)
+// }
+
+// for(var oN = 1; oN <= 10; oN +=2){
+//     console.log(oN)
+// }
+
+// var irregularNumber = [12, 4, 24, 56, 96, 678, 58, 19,69];
+// var indexNumber = irregularNumber.indexOf(56);
+// console.log(indexNumber);
+
+// var irregularNumber = [12, 4, 24, 56, 96, 678, 58, 19,69];
+// var object = irregularNumber[2]
+// console.log(object);
+
+// var irregularNumber = [12, 4, 24, 56, 96, 678, 58, 19,69];
+// for(var i = 0; i < irregularNumber.length; i++){
+//     console.log(i);
+// }
+
+// var irregularNumber = [12, 4, 24, 56, 96, 678, 58, 19,69];
+// for(var i = 0; i < irregularNumber.length; i++){
+//     var element = irregularNumber[i];
+//     console.log(element);
+// }
+// ভালো বুঝতে পারি নাই
+
+// var irregularNumber = [12, 4, 24, 56, 96, 678, 58, 19,69];
+// for(var i = 0; i < irregularNumber.length; i++){
+//     console.log(irregularNumber[i]);
+// }
+
+// how to loop unlimited number
